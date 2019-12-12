c32b39f908af00f89bd43d890d38a9f08f7b110a
Fred Morrison <fpmorrison@verizon.net>
2019-12-12 17:08:25 -0500

    remove debug code

--- ArrayTester.java
+++ ArrayTester.java
@@ -10,12 +10,9 @@ public class ArrayTester
 
 	public static int[] getColumn(int[][] arr2D, int c)
 	{
-		System.out.println("arr2D.length = " + arr2D.length);
-		System.out.println("arr2D[0].length = " + arr2D[0].length);
 		int[] result = new int[arr2D.length];
 		for (int row = 0; row < arr2D.length; row++)
-		{
-			System.out.println("row " + row + ": " + Arrays.toString(arr2D[row]));
+		{	
 			result[row] = arr2D[row][c];
 		}
 
